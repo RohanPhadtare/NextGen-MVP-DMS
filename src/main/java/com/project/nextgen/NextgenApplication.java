3d9c2ff1ef9e019452750981a1725e22c19d32aa
Rohan Phadtare <rohanmphadtare@gmail.com>
2026-03-10 14:29:24 +0530

    first commit

---
+++ src/main/java/com/project/nextgen/NextgenApplication.java
@@ -0,0 +1,13 @@
+package com.project.nextgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextgenApplication.class, args);
+	}
+
+}
